d27aec82d6c6ad2df1889a5acb047a63bb5c6cde
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-08-12 12:33:29 +0200

    Initial setup

---
+++ src/main/java/com/book/doctor/docbook/DocbookApplication.java
@@ -0,0 +1,13 @@
+package com.book.doctor.docbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/book/doctor/docbook/DocbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.doctor.docbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
