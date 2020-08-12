075ce8c69b47f7b31d08d459dd75ec3d679044ce
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-08-12 13:49:09 +0200

    Created multi-module maven project. close #1

--- docbook-web/src/main/java/com/book/doctor/docbook/DocbookApplication.java
+++ docbook-web/src/main/java/com/book/doctor/docbook/DocbookApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DocbookApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(DocbookApplication.class, args);
+        SpringApplication.run(DocbookApplication.class);
     }
-
 }
